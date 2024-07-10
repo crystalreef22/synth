0bf1f74e7cc13da8fd75c94c6168276456bdc095
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-07-10 11:39:10 -0400

    finished FIR filter?

--- fir.cpp
+++ fir.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <vector>
-#include "circular_buffer.hpp"
 //adapted from https://ptolemy.berkeley.edu/eecs20/week12/implementation.html
 
 // use vector, performance is not THAT important
 class FIRFilter {
 public:
 	FIRFilter(const std::vector<float>& impulseResponse)
-		: impulseResponse(impulseResponse) {}
+		: impulseResponse(impulseResponse),
+		delayLine(impulseResponse.size(), 0) // initialize delay line(circular buffer) to size of impulse response
+	{/* empty constructor */}
+
+	float filterSize(){
+		return impulseResponse.size();
+	}
 
 	float getOutputSample(float inputSample) {
 		delayLine[count] = inputSample;
 		float result = 0.0f;
-		int index = count;
-		//stub
-
+		size_t index = count;
+		for (size_t i=0; i<filterSize(); i++) {
+			result += impulseResponse[i] * delayLine[index--];
+			if (index < 0) index = filterSize()-1; //wrap around circular buffer
+		}
+		if (++count >= filterSize()) count = 0;
 		return result;
+
 	}
 private:
-	std::vector<float> impulseResponse;
+	const std::vector<float> impulseResponse;
 	std::vector<float> delayLine;
 	size_t count = 0;
 };
