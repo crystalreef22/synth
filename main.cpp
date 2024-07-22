87bb568a8c522ac5b9bc9ae09874055b1209c51c
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-07-22 11:32:39 -0400

    Made main.cpp read from file and play LPC

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <portaudio.h>
 #include "circular_buffer.hpp"
 #include <optional>
 #include <vector>
+#include <sstream>
+// uncomment to disable assert()
+// #define NDEBUG
+#include <cassert>
 // #include "fir.hpp"
 
 #define SAMPLE_RATE   (44100)
@@ -21,7 +26,7 @@
 static int paCallback( const void *inputBuffer, void *outputBuffer,
                            unsigned long framesPerBuffer,
                            const PaStreamCallbackTimeInfo* timeInfo,
-                           PaStreamCallbackFlags statusFlags,
+PaStreamCallbackFlags statusFlags,
                            void *userData )
 {
     shared_circular_buffer<float,RINGBUFFER_SIZE> *sharedBuffer = static_cast<shared_circular_buffer<float, RINGBUFFER_SIZE>*>(userData); 
@@ -37,7 +42,6 @@ static int paCallback( const void *inputBuffer, void *outputBuffer,
     return 0;
 }
 
-
 // Generate LPC for to read
 
 float genSaw(float x, float amplitude, float period) {
@@ -50,15 +54,18 @@ float genBuzz(float x, float noiseAmplitude, float sawAmplitude, float period) {
 
 class synth {
 public:
-    synth(const std::vector<float>& coefficients, float gain)
-        : coefficients(coefficients),
-        delayLine(coefficients.size(), 0),
-        gain{gain}
+    synth(size_t coefficientSize)
+        : coefficientSize(coefficientSize),
+        coefficients(coefficientSize, 0),
+        delayLine(coefficientSize, 0)
     {}
 
-    void setCoefficients(std::vector<float> coeff, float g){
-        coefficients = coeff;
-        gain = g;
+
+    void setCoefficients(const std::vector<float>& coeff, float g){
+        if (coeff.size() == coefficientSize){
+            coefficients = coeff;
+            gain = g;
+        }
     }
 
 
@@ -80,21 +87,110 @@ public:
         delayLine[count] = output;
         
         
-        
+
         return(std::max(-1.0f,std::min(output * sqrt(gain),1.0f)));
     }
 
 private:
     std::vector<float> coefficients;
     std::vector<float> delayLine;
+    size_t coefficientSize;
     size_t count = 0;
-    float gain;
+    float gain = 0;
 };
 
 
 int main(){
     // Read LPC
 
+
+    std::ifstream lpcFile("aeiou.LPC");
+    if (!lpcFile.is_open()){
+        std::cerr << "Error opening lpc file" << std::endl;
+        return 1;
+    }
+    std::string line;
+
+    std::getline(lpcFile, line);
+    if (line != "File type = \"ooTextFile\"") {
+        std::cerr << "Does not seem to be a LPC file" << std::endl;
+        return 1;
+    }
+
+    std::getline(lpcFile, line);
+    if (line != "Object class = \"LPC 1\"") {
+        std::cerr << "Does not seem to be a LPC file" << std::endl;
+        return 1;
+    }
+
+    float lpcFrameLength{0.02f};
+    
+    struct frame_t {
+        std::vector<float> coefficients;
+        float gain = 0;
+    };
+
+    std::vector<frame_t> lpcFrames;
+
+
+    while (true) {
+        // std::replace(line.begin(), line.end(), '=', ' ');
+        std::getline(lpcFile, line);
+        std::istringstream iss(line);
+        std::string varName;
+        iss >> varName;
+
+        if (varName == "frames"){
+            break;
+        } else if (varName == "dx"){
+            iss.ignore(128,'='); // 128 can be any arbitrary number
+
+            iss >> lpcFrameLength;
+        }
+    }
+
+    // assert(line == "frames []: ");
+    std::getline(lpcFile, line);
+    // assert(line == "    frames [1]:");
+    
+
+    bool keepReading = true;
+    while (keepReading) {
+        frame_t frame;
+        while (true) {
+            if (!std::getline(lpcFile, line)) {
+                keepReading = false;
+                break;
+            }
+            std::istringstream iss(line);
+            std::string varName;
+            iss >> varName;
+            
+
+            if (varName == "a"){
+                std::string token;
+                iss >> token;
+                if (token != "[]:") {
+                    iss.ignore(128,'=');
+                    float sample;
+                    iss >> sample;
+                    frame.coefficients.push_back(sample);
+                }
+                
+            } else if (varName == "gain") {
+                iss.ignore(128,'=');
+                iss >> frame.gain;
+            } else if (varName == "frames"){
+                break;
+            }
+        }
+        //std::cout << "fc" << std::endl;
+        //std::cout << frame.coefficients[0] << std::endl;
+        lpcFrames.push_back(frame);
+        //std::cin.ignore();
+    }
+
+    
 
 
 
@@ -135,13 +231,16 @@ int main(){
     if( err != paNoError ) goto error;
 
     {
-        synth mySynth({-0.8579094422019475,-0.9277631143195522,0.804326386102418,0.26789408269619314,-0.39655431995016505,0.40583070034300345,-0.04803689569700615,-0.7224031368285665,0.3706332655071031,0.5274388251363206,-0.5919288151596237,-0.2548778925565867,0.5521411691507471,0.1196527490841037,-0.26917557222963295,0.07046663444708721},0.00034088200960689826);
+        // synth mySynth({-0.8579094422019475,-0.9277631143195522,0.804326386102418,0.26789408269619314,-0.39655431995016505,0.40583070034300345,-0.04803689569700615,-0.7224031368285665,0.3706332655071031,0.5274388251363206,-0.5919288151596237,-0.2548778925565867,0.5521411691507471,0.1196527490841037,-0.26917557222963295,0.07046663444708721},0.00034088200960689826);
+        synth mySynth(lpcFrames[0].coefficients.size());
 
-        for(size_t i=0;i<SAMPLE_RATE*2;i++){
-            sharedBuffer.wait_put(mySynth.getOutputSample(i));
-        }
-        mySynth.setCoefficients({0}, 1);
-        for(size_t i=0;i<SAMPLE_RATE*2;i++){
+        size_t samplesPerFrame = lpcFrameLength*SAMPLE_RATE;
+        size_t sampleCount = lpcFrames.size()*samplesPerFrame;
+        for(size_t i=0;i<sampleCount;i++){
+            frame_t frame = lpcFrames[i/samplesPerFrame];
+            // std::cout << frame.gain << std::endl;
+            mySynth.setCoefficients(frame.coefficients, frame.gain);
+            //std::cout << frame.coefficients[0] << std::endl;
             sharedBuffer.wait_put(mySynth.getOutputSample(i));
         }
     }
